c11f6952bc44beaba0fad7a545bf5a19e0447fa4
Samanta Caroline <samantacarolinesantos@gmail.com>
2026-06-16 10:37:36 -0300

    Update carteira3.py

--- carteira3.py
+++ carteira3.py
@@ -2,22 +2,21 @@ class Carteira:
     def __init__(self, moeda, saldo):
         self.moedas = moeda
         self.saldo = saldo
-    isin
-   
-  
-        print(f'Adicionando {valor.nome}...', end='')
-        if chave and chave < len(self):
-            self.itens[chave] = valor
-            print('pronto!')
-        elif chave >= self.capacidade:
-            print('Sua bolsa está cheia!')
-        else:
-            self.itens.append(valor)
-            print('pronto!')
-
-    # Método especial para verificar se um item está na bolsa
-    # item in bag
-    def __contains__(self, item):
-        for potion in self.itens:
-            if potion.nome == item: return True
-        return False
+    def __add__ (self, other):
+        if isinstance(other, CNY):
+            return saldo (f"o saldo agora é " self.saldo + self.other)
+        elif isinstance(other, USD):
+            saldo = other * 0.14
+            return saldo (f"o saldo agora é " self.saldo + self.other)
+        elif isinstance(other, BRL):
+            saldo = other * 0.85
+            return saldo (f"o saldo agora é " self.saldo + self.other)
+     def __sub__ (self, other):
+        if isinstance(other, CNY):
+            return saldo (f"o saldo agora é " self.saldo - self.other)
+        elif isinstance(other, USD):
+            saldo = other * 0.14
+            return saldo (f"o saldo agora é " self.saldo - self.other)
+        elif isinstance(other, BRL):
+            saldo = other * 0.85
+            return saldo (f"o saldo agora é " self.saldo - self.other)
