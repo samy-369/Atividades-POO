138776e704d1682d4ad26c0d8f9e022c944d2f0f
Samanta Caroline <samantacarolinesantos@gmail.com>
2026-06-22 14:44:30 -0300

    Update main_pessoa04.py

--- main_pessoa04.py
+++ main_pessoa04.py
@@ -1,1 +1,18 @@
 from pessoa04 import Pessoa 
+lst = []
+quantos = int(input("Quantas pessoas serão cadastradas?"))
+
+for i range(quantidade):
+  print(f"\n Pessoa {i + 1}")
+  nome = input("Qual o nome?")
+  altura = float(input("Qual a altura em metros?"))
+  pessoa = Pessoa(nome, altura)
+  lst.append(pessoa)
+
+print("\n A pessoa mais alta é:")
+print(max(lst))
+print("\n A pessoa mais baixa é:")
+print(min(lst))
+print("\n Pessoas ordenadas por altura:")
+for pessoa in sorted(lst):
+  print(pessoa)
