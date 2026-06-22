5064858b62d2f25b57caec82818f9551222acc4e
Samanta Caroline <samantacarolinesantos@gmail.com>
2026-06-22 15:16:12 -0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -2,3 +2,10 @@
 nome = input("Digite seu primeiro nome:")
 sobrenome = input("Digite seu sobrenome:")
 print (f"Bem-vinda, {nome} {sobrenome}")
+
+# questão 02
+frase = input("Digite a frase:)
+espacos = frase.count(" ")
+print("\n Espaços em branco: {espacos}" 
+
+# questão 03
