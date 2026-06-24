96cbe7ec7b7793056f51f3050c032002f7334e12
Samanta Caroline <samantacarolinesantos@gmail.com>
2026-06-24 16:40:48 -0300

    Update pessoa04.py

--- pessoa04.py
+++ pessoa04.py
@@ -1,10 +1,25 @@
 class Pessoa:
-    def __init__(self, nome, altura):
-        self.nome = nome
-        self.idade = altura
-    def __str__(self):
-        return f"Nome: {self.nome}, altura: {self.altura}"
-    def __gt__(self, outro):
-        return self.altura > outro.altura
-    def __lt__(self, outro):
-        return self.altura < outro.altura
+  def __init__(self, nome: str, altura: float):
+    self.nome = nome
+    self.altura = altura
+  def __str__(self):
+    return f'Seu nome: {self.nome}, sua altura: {self.altura}'
+  def __gt__(self, other):
+    return self.altura > other.altura
+  def __lt__(self, other):
+    return self.altura < other.altura
+
+qtd = int(input("quantas pessoas serão cadastradas?"))
+cad = []
+for i in range(qtd):
+  print(f"\n--- Cadastro {i + 1} ---")
+  nome = input("Digite o nome: ")
+  altura = float(input("altura: "))
+  pessoa = Pessoa(nome, altura)
+  cad.append(pessoa)
+
+print(f'Máximo: {max(cad)}')
+print(f'Mínimo: {min(cad)}')
+
+for pessoa in sorted(cad):
+  print(f'---- LISTA DE CADASTRO ---- {pessoa}')
